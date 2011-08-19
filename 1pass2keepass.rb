b036ed4a18afae3fd999231d8994201413759a39
Robert Liesenfeld <xunil@squareup.com>
2011-08-19 14:41:38 -0700

    Parse 1Password Interchange format files instead of CSV export.  Produce XML with groups that mirror 1Password types.

--- 1pass2keepass.rb
+++ 1pass2keepass.rb
@@ -14,17 +14,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
+require 'rubygems'
+require 'json'
 require "rexml/document"
 include REXML
-require 'csv'
+require 'awesome_print'
 
 def usage (message = nil)
   if message
     puts "ERROR: #{message}"
   end
-  puts """Usage: 1pass2keepass.rb 1pass.csv
+  puts """Usage: 1pass2keepass.rb 1pass.1pif
 
-Takes a _TAB_ delimeted csv file from 1password and prints XML suitable for import into keepassX
+Convert a 1Password export file to XML suitable for import into KeepassX.
 """
   exit
 end
@@ -38,53 +40,89 @@ unless File.exists?(input_file)
   usage "File '#{input_file}' does not exist" 
 end
 
-begin
-  csv_data = CSV.open(input_file, 'rb', "\t")
-  headers = csv_data.shift.map {|i| i.to_s }
-  string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
-  array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
-rescue
-  usage $!
-end
+lines = File.open(input_file).readlines()
+lines.reject! {|l| l =~ /^\*\*\*/}
 
-
-def usage (message = '')
-  if message
-    puts "ERROR: #{message}"
+groups = {}
+username = password = nil
+lines.each do |line|
+  entry = JSON.parse(line)
+  if entry['trashed']
+    next
   end
-  puts """ 
-  Usage: 1pass2keepass.rb 1pass.csv
-  Takes a _TAB_ delimeted csv file from 1password and prints XML suitable for import into keepassX
-"""
-  exit
+
+  group_name = entry['typeName'].split('.')[-1]
+  if not groups.has_key?(group_name)
+    groups[group_name] = {}
+  end
+  title = entry['title']
+
+  case group_name
+    when 'Password','Database','UnixServer','Email','GenericAccount'
+      groups[group_name][title] = {
+        :url => nil,
+        :username => entry['secureContents']['username'],
+        :password => entry['secureContents']['password'],
+      } unless (groups[group_name].has_key?(title) and groups[group_name][title]['updatedAt'].to_i > entry['updatedAt'].to_i)
+    when 'Regular', 'SavedSearch', 'Point'
+      next
+    when 'WebForm'
+      entry['secureContents']['fields'].each do |field|
+        case field['designation']
+          when 'username'
+            username = field['value']
+          when 'password'
+            password = field['value']
+        end
+      end
+
+      groups[group_name][title] = {
+        :url => entry['location'],
+        :username => username,
+        :password => password,
+      } unless (groups[group_name].has_key?(title) and groups[group_name][title]['updatedAt'].to_i > entry['updatedAt'].to_i)
+
+      username = password = nil
+    else
+      puts "Don't know how to handle records of type #{entry['typeName']} yet."
+  end
 end
 
 doc = Document.new 
 database = doc.add_element 'database'
-group = database.add_element 'group'
-group.add_element('title').text = 'Internet'
-group.add_element('icon').text = '1'
 
-
-def usage (message = '')
-  if message
-    puts "ERROR: #{message}"
+groups.each do |group_name, entries|
+  next if entries.empty?
+  group = database.add_element 'group'
+  case group_name
+    when 'Password'
+      group.add_element('title').text = 'Password'
+      group.add_element('icon').text = '0'
+    when 'WebForm'
+      group.add_element('title').text = 'Internet'
+      group.add_element('icon').text = '1'
+    when 'Email'
+      group.add_element('title').text = 'Email'
+      group.add_element('icon').text = '19'
+    when 'Database'
+      group.add_element('title').text = 'Database'
+      group.add_element('icon').text = '6'
+    when 'UnixServer'
+      group.add_element('title').text = 'Unix Server'
+      group.add_element('icon').text = '30'
+    when 'GenericAccount'
+      group.add_element('title').text = 'Generic Account'
+      group.add_element('icon').text = '20'
   end
-  puts """ 
-  Usage: 1pass2keepass.rb 1pass.csv
-  Takes a _TAB_ delimeted csv file from 1password and prints XML suitable for import into keepassX
-"""
-  exit
-end
 
-array_of_hashes.each do |row|
-  entryNode = group.add_element 'entry'
-  entryNode.add_element('username').text = row['username']
-  entryNode.add_element('password').text = row['password']
-  entryNode.add_element('title').text = row['title']
-  entryNode.add_element('url').text = row['URL/Location']
-  entryNode.add_element('comment').text = row['notes']
+  entries.each do |title, entry|
+    entry_node = group.add_element 'entry'
+    entry_node.add_element('username').text = entry[:username]
+    entry_node.add_element('password').text = entry[:password]
+    entry_node.add_element('title').text = title
+    entry_node.add_element('url').text = entry[:url]
+  end
 end
 
 doc << XMLDecl.new
-doc.write($stdout,2)
+doc.write($stdout)
